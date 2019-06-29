443f9a14de34fd145c54ad6bfc0723889602d874
AlanTuring <imjcker@qq.com>
2019-06-29 12:08:41 +0800

    first commit

---
+++ EveryDoc/System/KeyPressEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace System
+{
+    internal class KeyPressEventHandler
+    {
+        private Action<object, KeyEventArgs> textBoxSearch_TextChanged;
+
+        public KeyPressEventHandler(Action<object, KeyEventArgs> textBoxSearch_TextChanged)
+        {
+            this.textBoxSearch_TextChanged = textBoxSearch_TextChanged;
+        }
+    }
+}
