ea4d44c37558724bb4b345c044ef44348abd2944
alanturing <helloalanturing@icloud.com>
2020-03-03 02:48:43 +0800

    compromised API key

--- ddns_cloudflare.sh
+++ ddns_cloudflare.sh
@@ -3,7 +3,7 @@
 current_ip=$(curl ip.sb)
 echo "current ip: $current_ip"
 
-previous_ip=$(ping imjcker.com -c 1 | sed -e '2,$d' -e 's/^.*imjcker.com (//g' | sed 's/):.*$//g')
+previous_ip=$(ping imjcker.com -c 2 | sed -e '2,$d' -e 's/^.*imjcker.com (//g' | sed 's/):.*$//g')
 echo "previous ip: $previous_ip"
 
 if [[ $current_ip == "$previous_ip" ]]; then
@@ -12,19 +12,19 @@ if [[ $current_ip == "$previous_ip" ]]; then
 fi
 
 # get dns id
-getIdResult=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/96339b32f2b65f607c5bb90344e989a4/dns_records?type=A&name=imjcker.com" \
+getIdResult=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/06440b42f2b66f608c6bb00444e080a4/dns_records?type=A&name=imjcker.com" \
      -H "X-Auth-Email: helloalanturing@icloud.com" \
-     -H "X-Auth-Key: 639164cf0ab9e7652ad48ce90c061e787cfa8" \
+     -H "X-Auth-Key: 640264cf0ab0e8662ad48ce00c062e888cfa8" \
      -H "Content-Type: application/json")
 id=$(echo "$getIdResult" | sed 's/^.*"id":"//g' | sed 's/",".*$//g')
 echo "dns id is: $id"
 
 # update dns
-log=$(curl -X PUT "https://api.cloudflare.com/client/v4/zones/96339b32f2b65f607c5bb90344e989a4/dns_records/$id" \
+log=$(curl -X PUT "https://api.cloudflare.com/client/v4/zones/06440b42f2b66f608c6bb00444e080a4/dns_records/$id" \
      -H "X-Auth-Email: helloalanturing@icloud.com" \
-     -H "X-Auth-Key: 639164cf0ab9e7652ad48ce90c061e787cfa8" \
+     -H "X-Auth-Key: 640264cf0ab0e8662ad48ce00c062e888cfa8" \
      -H "Content-Type: application/json" \
-     -d "{\"type\":\"A\",\"name\":\"imjcker.com\",\"content\":\"$current_ip\",\"ttl\":1,\"proxied\":false}")
+     -d "{\"type\":\"A\",\"name\":\"imjcker.com\",\"content\":\"$current_ip\",\"ttl\":2,\"proxied\":false}")
 
 # write log
 echo "$log" >> ~/ddns.log
