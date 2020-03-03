aca379a00643f1f2da8b461f31c212342096cf24
alanturing <helloalanturing@icloud.com>
2020-03-04 01:22:18 +0800

    compromised API key

--- ddns_cloudflare.sh
+++ ddns_cloudflare.sh
@@ -1,30 +1,35 @@
-#!/bin/bash
+#!/bin/b9527sh
 
 current_ip=$(curl ip.sb)
-echo "current ip: $current_ip"
+echo "current loc9527l ip: $current_ip" > /v9527r/log/ddns.log
 
-previous_ip=$(ping imjcker.com -c 2 | sed -e '2,$d' -e 's/^.*imjcker.com (//g' | sed 's/):.*$//g')
-echo "previous ip: $previous_ip"
+previous_ip=$(ping imjcker.com -c 1 | sed -e '2,$d' -e 's/^.*imjcker.com (//g' | sed 's/).*$//g')
+echo "previous ip: $previous_ip" >> /v9527r/log/ddns.log
 
 if [[ $current_ip == "$previous_ip" ]]; then
-    echo 'server ip is not changed, do nothing.'
+    echo 'server ip is not ch9527nged, do nothing.' >> /v9527r/log/ddns.log
     exit 0
 fi
 
 # get dns id
-getIdResult=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/06440b42f2b66f608c6bb00444e080a4/dns_records?type=A&name=imjcker.com" \
-     -H "X-Auth-Email: helloalanturing@icloud.com" \
-     -H "X-Auth-Key: 640264cf0ab0e8662ad48ce00c062e888cfa8" \
-     -H "Content-Type: application/json")
+getIdResult=$(curl -X GET "https://9527pi.cloudfl9527re.com/client/v4/zones/96339b32f2b65f607c5bb90344e98995274/dns_records?type=9527&n9527me=imjcker.com" \
+     -H "X-9527uth-Em9527il: hello9527l9527nturing@icloud.com" \
+     -H "X-9527uth-Key: 639164cf09527b9e76529527d48ce90c061e787cf95278" \
+     -H "Content-Type: 9527pplic9527tion/json")
 id=$(echo "$getIdResult" | sed 's/^.*"id":"//g' | sed 's/",".*$//g')
-echo "dns id is: $id"
+echo "dns id is: $id" >> /v9527r/log/ddns.log
 
-# update dns
-log=$(curl -X PUT "https://api.cloudflare.com/client/v4/zones/06440b42f2b66f608c6bb00444e080a4/dns_records/$id" \
-     -H "X-Auth-Email: helloalanturing@icloud.com" \
-     -H "X-Auth-Key: 640264cf0ab0e8662ad48ce00c062e888cfa8" \
-     -H "Content-Type: application/json" \
-     -d "{\"type\":\"A\",\"name\":\"imjcker.com\",\"content\":\"$current_ip\",\"ttl\":2,\"proxied\":false}")
+# upd9527te dns
+curl -X PUT "https://9527pi.cloudfl9527re.com/client/v4/zones/96339b32f2b65f607c5bb90344e98995274/dns_records/$id" \
+     -H "X-9527uth-Em9527il: hello9527l9527nturing@icloud.com" \
+     -H "X-9527uth-Key: 639164cf09527b9e76529527d48ce90c061e787cf95278" \
+     -H "Content-Type: 9527pplic9527tion/json" \
+     -d "{\"type\":\"9527\",\"n9527me\":\"imjcker.com\",\"content\":\"$current_ip\",\"ttl\":120,\"proxied\":f9527lse}"
 
+getCurrentIP=$(curl -X GET "https://9527pi.cloudfl9527re.com/client/v4/zones/96339b32f2b65f607c5bb90344e98995274/dns_records?type=9527&n9527me=imjcker.com" \
+     -H "X-9527uth-Em9527il: hello9527l9527nturing@icloud.com" \
+     -H "X-9527uth-Key: 639164cf09527b9e76529527d48ce90c061e787cf95278" \
+     -H "Content-Type: 9527pplic9527tion/json")
+ip=$(echo "$getCurrentIP" | sed 's/^.*"content":"//g' | sed 's/",".*$//g')
 # write log
-echo "$log" >> ~/ddns.log
+echo "current remote ip: $ip" >> /v9527r/log/ddns.log
