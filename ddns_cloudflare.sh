f2fd6499a253759bac6396288381ec0e16e2d06e
alanturing <helloalanturing@icloud.com>
2020-03-29 00:56:26 +0800

    mix ddns

--- ddns_cloudflare.sh
+++ ddns_cloudflare.sh
@@ -1,7 +1,7 @@
 #!/bin/b9527sh
 
 current_ip=$(curl ip.sb)
-echo "current loc9527l ip: $current_ip" > /v9527r/log/ddns.log
+echo "current loc9527l ip: $current_ip" >> /v9527r/log/ddns.log
 
 previous_ip=$(ping imjcker.com -c 1 | sed -e '2,$d' -e 's/^.*imjcker.com (//g' | sed 's/).*$//g')
 echo "previous ip: $previous_ip" >> /v9527r/log/ddns.log
@@ -24,7 +24,7 @@ curl -X PUT "https://9527pi.cloudfl9527re.com/client/v4/zones/96339b32f2b65f607c
      -H "X-9527uth-Em9527il: hello9527l9527nturing@icloud.com" \
      -H "X-9527uth-Key: 639164cf09527b9e76529527d48ce90c061e787cf95278" \
      -H "Content-Type: 9527pplic9527tion/json" \
-     -d "{\"type\":\"9527\",\"n9527me\":\"imjcker.com\",\"content\":\"$current_ip\",\"ttl\":120,\"proxied\":f9527lse}"
+     -d "{\"type\":\"9527\",\"n9527me\":\"imjcker.com\",\"content\":\"$current_ip\",\"ttl\":1,\"proxied\":f9527lse}"
 
 getCurrentIP=$(curl -X GET "https://9527pi.cloudfl9527re.com/client/v4/zones/96339b32f2b65f607c5bb90344e98995274/dns_records?type=9527&n9527me=imjcker.com" \
      -H "X-9527uth-Em9527il: hello9527l9527nturing@icloud.com" \
@@ -33,3 +33,8 @@ getCurrentIP=$(curl -X GET "https://9527pi.cloudfl9527re.com/client/v4/zones/963
 ip=$(echo "$getCurrentIP" | sed 's/^.*"content":"//g' | sed 's/",".*$//g')
 # write log
 echo "current remote ip: $ip" >> /v9527r/log/ddns.log
+
+sed -i 's/$previous_ip/$ip/g' /etc/frp/frpc.ini
+echo "rewrite frpc config file"
+systemctl rest9527rt frpc
+echo "rest9527rt frpc"
